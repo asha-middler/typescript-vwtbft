5a57962dd1825980f1a623299e8aab0b191f7dfb
asha-middler <51950985+asha-middler@users.noreply.github.com>
2023-08-28 21:54:02 +0200

    todo list zkouska

--- index.ts
+++ index.ts
@@ -1,13 +1,36 @@
 // Import stylesheets
 import './style.css';
-//import axios from 'axios';
+import axios from 'axios';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 var value: String = 'HAHAHA';
 console.log(`${value}s`);
-appDiv.innerHTML = `<h1>TypeScript Starter ${value}</h1>`;
+appDiv.innerHTML = `<h1>TypeScript Starter ${value} -- hele 2</h1>`;
 
-// axios.get("127.0.0.1").then( (response) => {
-//     appDiv.innerHTML= `DATA = ${response.data}`;
-// });
+interface Todo {
+  id: number;
+  todo: string;
+  completed: boolean;
+  userId: number;
+}
+
+interface Todos {
+  todos: Todo[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+
+axios.get('https://dummyjson.com/todos').then((response) => {
+  const todos: Todos = response.data as Todos;
+
+  var out = '<ul>';
+
+  for (let todo of todos.todos) {
+    out += `<li>${todo.id} - ${todo.todo}</li>`;
+  }
+
+  out += '</ul>';
+  appDiv.innerHTML = `${out}`;
+});
